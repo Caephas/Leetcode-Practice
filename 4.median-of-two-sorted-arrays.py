d41da1a68c01bbc787797eec1a16ba1b0678b67b
Caephas <arinzeobidiegwu@gmail.com>
2025-01-27 12:51:46 +0000

    minor: median of two sorted arrays

--- 4.median-of-two-sorted-arrays.py
+++ 4.median-of-two-sorted-arrays.py
@@ -6,7 +6,26 @@
 
 # @lc code=start
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        merged_array = nums1 + nums2
+        merged_array.sort()
+        n = len(merged_array)
+        median_index = n // 2
+
+        if n % 2 == 0:
+            median = (merged_array[median_index - 1] + merged_array[median_index]) / 2
+        else:
+            median = merged_array[median_index]
+        return median
+
         
 # @lc code=end
 
+sol_class = Solution()
+sol_class.findMedianSortedArrays([1,3],[2])
+
+"""
+The median index represents the position of the middle element(s) in the array. 
+Subtracting 1 from it gives us the index of the previous element (to the left of the middle),
+which is needed when the total number of elements is even.
+"""
